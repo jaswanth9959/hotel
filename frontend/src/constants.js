a45e114aa5bbe1cbc37c4fa9cae628c6a7866549
jaswanth9959 <60003589+jaswanth9959@users.noreply.github.com>
2024-06-05 20:52:11 -0500

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+export const BASE_URL ="";
 
 export const ROOMS_URL = "/api/rooms";
 export const USERS_URL = "/api/users";
